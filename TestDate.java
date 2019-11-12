6e0703bc06fe28a8b8f6f961fde4141bc959e2a5
Laurent Rous <rouslaurent@gmail.com>
2019-11-12 10:05:46 +0100

    ok

--- TestDate.java
+++ TestDate.java
@@ -18,6 +18,11 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year, month-1, day);
+        Date d = c.getTime();
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The date is : " + sdf.format(d));
+        System.out.println("The day of week is : " + dayOfWeek);
     }
 }
